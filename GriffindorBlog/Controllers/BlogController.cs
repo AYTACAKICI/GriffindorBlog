058e3f5c45c3513fa2e6c68e5ab791cd66d8e8c0
lenovo <mr.yildirimarda@gmail.com>
2023-09-22 00:29:49 +0300

    Son durum...

--- GriffindorBlog/Controllers/BlogController.cs
+++ GriffindorBlog/Controllers/BlogController.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GriffindorBlog.Database.Entities;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GriffindorBlog.DAL.Interfaces;
 
 namespace GriffindorBlog.Controllers
 {
     public class BlogController : Controller
     {
+        private readonly IBlokDAL _blokdal;
+        public BlogController(IBlokDAL blokdal)
+        {
+            _blokdal = blokdal;
+        }
 
         public IActionResult Index()
         {
@@ -27,19 +34,41 @@ namespace GriffindorBlog.Controllers
             return View();
         }
         [HttpPost]
-        //public IActionResult Login()
-        //{
-        //    return View();
-        //}
+        public IActionResult Login(User user)
+        {
+         
+           
+            if (ModelState.IsValid)
+            {
+             _blokdal.Login(user.UserName, user.Password);
+               
+            }
+            if (user != null)
+            {
+                return RedirectToAction("About");
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+
+
+        }
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
 
-        //[HttpPost]
-        //public IActionResult Register()
-        //{
-        //    return View();
-        //}
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            
+            if(ModelState.IsValid)
+            {
+                _blokdal.Register(user);
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
